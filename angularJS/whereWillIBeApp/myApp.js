31924dc865b1681142fff58076f2698608af4432
David Crespo <davidcrespoarroyo@gmail.com>
2016-05-18 12:53:30 +0100

    Added style. Added some html elements

--- angularJS/whereWillIBeApp/myApp.js
+++ angularJS/whereWillIBeApp/myApp.js
@@ -1,7 +1,1 @@
 var app = angular.module('myApp', []);
-
-app.directive("myCustomDirective", function() {
-	return {
-		template: "This is my custom directive"
-	};
-});
--- angularJS/whereWillIBeApp/myCtrl.js
+++ angularJS/whereWillIBeApp/myCtrl.js
@@ -1,5 +1,3 @@
 app.controller('myCtrl', function($scope) {
-			var d = new Date();
-    		var n = d.toUTCString();
-		    $scope.time= n;
-		});
+	
+});
